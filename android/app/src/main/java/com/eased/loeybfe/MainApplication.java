0c2a66b544a9d5f4b8f1e1ec9ab9d7caa2188d98
thimtkfkd05 <thimtkfkd05@gmail.com>
2022-07-07 22:50:34 +0900

    update RN version to 0.68.2

--- android/app/src/main/java/com/eased/loeybfe/MainApplication.java
+++ android/app/src/main/java/com/eased/loeybfe/MainApplication.java
@@ -6,10 +6,15 @@ import android.content.Context;
 import com.eased.loeybfe.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.surajit.rnrg.RNRadialGradientPackage;
+import com.surajit.rnrg.RNRadialGradientPackage;
+import com.surajit.rnrg.RNRadialGradientPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.eased.loeybfe.newarchitecture.MainApplicationReactNativeHost;
 import com.reactnativegooglesignin.RNGoogleSigninPackage;
 
 import java.lang.reflect.InvocationTargetException;
@@ -40,14 +45,23 @@ public class MainApplication extends Application implements ReactApplication {
         }
       };
 
+  private final ReactNativeHost mNewArchitectureNativeHost =
+      new MainApplicationReactNativeHost(this);
+
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
+      return mReactNativeHost;
+    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
